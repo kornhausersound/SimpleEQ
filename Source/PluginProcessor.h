be77c0d0f00e3765ccd6e04a7bcabac20b9fd336
kornhausersound <112279112+kornhausersound@users.noreply.github.com>
2022-09-21 00:37:31 -0400

    Connected peak params to filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,6 +10,16 @@
 
 #include <JuceHeader.h>
 
+struct ChainSettings
+{
+    float peakFreq { 0 }, peakGainInDecibels{ 0 }, peakQuality {1.0f};
+    float lowCutFreq { 0 }, highCutFreq { 0 };
+    int lowCutSlope { 0 }, highCutSlope { 0 };
+};
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
+
 //==============================================================================
 /**
 */
@@ -68,7 +78,12 @@ private:
     
     MonoChain leftChain, rightChain;
     
-    
+    enum ChainPositions
+    {
+        LowCut,
+        Peak,
+        HighCut
+    };
     
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
