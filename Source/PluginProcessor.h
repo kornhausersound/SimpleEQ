b302015ce6bf3a2ba2066a8757093b95e9456ca0
kornhausersound <112279112+kornhausersound@users.noreply.github.com>
2022-10-01 18:34:05 -0400

    D.R.Y refactored DSP filter configuration

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -119,7 +119,7 @@ private:
 
         switch( slope )
         {
-            case Slope_48: 
+            case Slope_48:
             {
                  update<3>(chain, coefficients);
             }
@@ -139,6 +139,10 @@ private:
         }
     }
     
+    void updateLowCutFilters(const ChainSettings& chainSettings);
+    void updateHighCutFilters(const ChainSettings& chainSettings);
+
+    void updateFilters();
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
